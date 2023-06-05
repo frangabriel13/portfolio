10e629e8b7de8a841b0a51c0c0ae15e6b0dd0ca4
franco <mansilla.franco.1@gmail.com>
2023-06-04 21:15:38 -0300

    header terminado

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import s from "./Header.module.css"
+import { useState } from "react";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className={s.container}>
         <a href="#home" className={s.logo}>Franco.</a>
-        <nav className={s.navbar}>
+        <nav className={`${s.navbar} ${menuOpen ? s.open : ''}`}>
             <a href="#home" className={s.home}>Home</a>
             <a href="#about">About</a>
             <a href="#skills">Skills</a>
@@ -13,7 +20,7 @@ function Header() {
             <a href="#contact">Contact</a>
         </nav>
         <div className={s.navToggle}>
-          <i className={`bx bx-menu ${s.icon}`}></i>
+          <i className={`bx bx-menu ${s.icon}`} onClick={toggleMenu}></i>
         </div>
     </div>
   );
