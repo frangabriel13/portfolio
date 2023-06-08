09f32138a395957b72cbb0dda615d5ed044c88df
franco <mansilla.franco.1@gmail.com>
2023-06-08 15:51:10 -0300

    skill hecho

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import s from "./Skills.module.css";
+
+const Skills = () => {
+  const skills = ["JavaScript", "TypeScript", "WordPress", "HTML", "CSS", "Node.js", 
+    "Express", "Sequelize", "PostgreSQL", "MongoDB", "Mongoose", "MySQL", "React", 
+    "React Native", "Redux", "Bootstrap", "Tailwind"];
+  
+  return (
+    <div className={s.container}>
+      <h2 className={s.title}>Skills</h2>
+      <div className={s.skills}>
+        {skills.map((skill, index) => (
+          <span key={index} className={s.skill}>{skill}</span>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
