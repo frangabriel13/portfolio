099d40d218f69e2d2d0cf25f0e4081327af33dd8
franco <mansilla.franco.1@gmail.com>
2023-06-09 18:00:56 -0300

    projects hecho

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,10 +1,38 @@
+// import React from "react";
+// import s from "./ProjectCard.module.css";
+
+// const ProjectCard = ({ image, description, title, isOpen, onToggleDescription }) => {
+//   return (
+//     <div className={s.card}>
+//       <img src={image} alt="Project" className={s.image} />
+//       {isOpen && (
+//         <div className={s.description}>
+//           <h3>{title}</h3>
+//           <p>{description}</p>
+//         </div>
+//       )}
+//       <button className={s.button} onClick={onToggleDescription}>
+//         {isOpen ? "Ver menos" : "Ver más"}
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default ProjectCard;
+
 import React from "react";
 import s from "./ProjectCard.module.css";
 
 const ProjectCard = ({ image, description, title, isOpen, onToggleDescription }) => {
   return (
     <div className={s.card}>
-      <img src={image} alt="Project" className={s.image} />
+      <div className={s.imageContainer}>
+        <img src={image} alt="Project" className={s.image} />
+        <div className={s.iconContainer}>
+          <i className="bx bx-world"></i>
+          <i className="bx bxl-github"></i>
+        </div>
+      </div>
       {isOpen && (
         <div className={s.description}>
           <h3>{title}</h3>
