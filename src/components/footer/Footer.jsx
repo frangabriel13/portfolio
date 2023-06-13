92e1f945d87f437a7fa892d03b7ecc1370e24824
franco <mansilla.franco.1@gmail.com>
2023-06-13 18:12:41 -0300

    footer terminado

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import s from './Footer.module.css';
 
 const Footer = () => {
+  const handleMenuClick = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div>
       <div className={s.container}>
         <div className={s.menu}>
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Projects</li>
-            <li>Contact</li>
+            <li onClick={() => handleMenuClick('home')}>Home</li>
+            <li onClick={() => handleMenuClick('about')}>About</li>
+            <li onClick={() => handleMenuClick('skills')}>Skills</li>
+            <li onClick={() => handleMenuClick('projects')}>Projects</li>
+            <li onClick={() => handleMenuClick('contact')}>Contact</li>
           </ul>
         </div>
         <div className={s.copy}>
